df3518e10fc84afed55a40696b2470091e31363a
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-05-11 16:51:33 +0530

    Added efficient solution for Jesse_and_cookies.java

--- Jesse_and_cookies.java
+++ Jesse_and_cookies.java
@@ -5,6 +5,75 @@ import java.util.List;
  * @author Yudhishthir Singh. Bangalore. 2020
  */
 
+
+//The code below is written in JAVA 7
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+    static int cookies(int k, int[] A) {
+
+        if(A.length == 0 ||A.length == 1) return 0;
+        if(A.length == 2) {
+            if(A[0] + A[1] < k) return 1;
+            else return 0;
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int item : A) pq.add(item);
+
+        //At this point, we have a heap set up with the help of priority queue.
+        int counter = 0;
+        while(pq.size() > 1) {
+            if(pq.peek() >= k){
+                return counter;
+            }
+            else{
+                int lastElement = pq.poll(); //this will remove the last element and will return it's value to us
+                int secondLastElement = pq.poll(); // same STORY
+                int newElement = lastElement + (2*secondLastElement);
+                pq.add(newElement);
+                counter++;
+            }
+        }
+        if(pq.peek() < k) return -1;
+        return counter;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0].trim());
+
+        int k = Integer.parseInt(nk[1].trim());
+
+        int[] A = new int[n];
+
+        String[] AItems = scanner.nextLine().split(" ");
+
+        for (int AItr = 0; AItr < n; AItr++) {
+            int AItem = Integer.parseInt(AItems[AItr].trim());
+            A[AItr] = AItem;
+        }
+
+        int result = cookies(k, A);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+    }
+}
+
+
 //BRUTE FORCE APPROACH: DO NOT DO THIS WAY
 //Approach: keep sorting the array and converting the last two elements with the given formula to a sweeter cookie until the last cookie is sweeter than the threshold value.
 //public class Jesse_and_cookies {
